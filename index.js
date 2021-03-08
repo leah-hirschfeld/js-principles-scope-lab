d379607b5512544e0a620039c8f3bdfbeb3d18ab
leah_hirschfeld <leah.hirschfeld@gmail.com>
2021-03-07 20:26:15 -0800

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var  customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName 
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    console.log(bestCustomer);
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    console.log(bestCustomer);
+}
+
+const leastFavoriteCustomer = 'still bob';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'now bob'
+    console.log(leastFavoriteCustomer);
+}
